c5ebf4bad0afd622a0c237d90a353f179757ac6e
Alberto Otero Lorenzo <aoterolorenzo@gmail.com>
2021-11-22 17:42:49 +0000

    Resolve "Add support for internal settings overriding"

--- config/settings.go
+++ config/settings.go
@@ -1,8 +1,11 @@
 package settings
 
 import (
+	"fmt"
+	"github.com/imdario/mergo"
 	"gitlab.com/aoterocom/changelog-guardian/application/models"
 	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"log"
 )
 
@@ -16,7 +19,8 @@ var Settings = &GlobalSettings{}
 type GlobalSettings struct {
 	ChangelogPath string `yaml:"changelogPath"`
 	MainBranch    string `yaml:"mainBranch"`
-	DevelopBranch string `yaml:"developBranch"`
+	DevelopBranch string `yaml:"defaultBranch"`
+	CGConfigPath  string `yaml:"cgConfigPath"`
 	Providers     struct {
 		Gitlab struct {
 			Labels map[models.Category]string `yaml:"labels"`
@@ -25,12 +29,33 @@ type GlobalSettings struct {
 }
 
 func init() {
+	fmt.Println("Constructing internal settings...")
 	if err := extractSettings(settingsFile); err != nil {
 		log.Panicln(err)
 	}
+
+	fmt.Println("Retrieving settings from " + Settings.CGConfigPath + "...")
+	yamlFile, err := ioutil.ReadFile(Settings.CGConfigPath)
+	if err != nil {
+		log.Printf("Unable to open "+Settings.CGConfigPath+": ", err)
+	} else {
+		if err := extractSettings(string(yamlFile)); err != nil {
+			log.Panicln(err)
+		}
+	}
+
+	fmt.Println("Settings successfully retrieved.")
 }
 
-func extractSettings(file string) error {
-	err := yaml.Unmarshal([]byte(file), Settings)
-	return err
+func extractSettings(content string) error {
+	var setts = &GlobalSettings{}
+	if err := yaml.Unmarshal([]byte(content), setts); err != nil {
+		return err
+	}
+
+	if err := mergo.Merge(Settings, setts, mergo.WithOverride); err != nil {
+		return err
+	}
+
+	return nil
 }
